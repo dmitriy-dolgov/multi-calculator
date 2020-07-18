08b934486ee2112eacfaa887350c7b8607652e72
dmitriy-dolgov <TwilightTower@mail.ru>
2020-07-18 19:04:03 +0300

    rbac added to backend

---
+++ backend/rbac/ClientRule.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace backend\rbac;
+
+use yii\rbac\Rule;
+//use app\models\db\Component;
+
+/**
+ * Checks if authorID matches user passed via params
+ */
+class ClientRule extends Rule
+{
+    public $name = 'isAuthor';
+
+    /**
+     * @param string|int $user the user ID.
+     * @param Item $item the role or permission that this rule is associated with
+     * @param array $params parameters passed to ManagerInterface::checkAccess().
+     * @return bool a value indicating whether the rule permits the role or permission it is associated with.
+     */
+    public function execute($user, $item, $params)
+    {
+        return isset($params['component']) ? $params['component']->user_id == $user : false;
+    }
+}

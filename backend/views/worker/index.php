f2a01ee8e391367f13e59e1122ea5dd047f1bb9a
dmitriy-dolgov <TwilightTower@mail.ru>
2020-08-05 18:51:16 +0300

    pulse icon for worker stuff

--- backend/views/worker/index.php
+++ backend/views/worker/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\Url;
 
 /* @var $this yii\web\View */
 /* @var $worker \common\models\db\CoWorker */
@@ -11,11 +12,13 @@ $this->registerCssFile('https://unpkg.com/leaflet@1.6.0/dist/leaflet.css', [
     'integrity' => 'sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==',
     'crossorigin' => '',
 ]);
-
 $this->registerJsFile('https://unpkg.com/leaflet@1.6.0/dist/leaflet.js', [
     'integrity' => 'sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew==',
     'crossorigin' => '',
 ]);
+
+$this->registerJsFile(Url::to(['/js/leaflet/L.Icon.Pulse.js']), ['depends' => ['frontend\assets\VendorAsset']]);
+$this->registerCssFile(Url::to(['/js/leaflet/L.Icon.Pulse.css']));
 
 $this->registerCss(<<<CSS
 #worker-place-map {
@@ -25,7 +28,7 @@ $this->registerCss(<<<CSS
 CSS
 );
 
-$this->registerJsFile(\yii\helpers\Url::to(['/js/worker/geo.js']), ['depends' => ['backend\assets\WorkerAsset']]);
+$this->registerJsFile(Url::to(['/js/worker/geo.js']), ['depends' => ['backend\assets\WorkerAsset']]);
 
 $this->registerJs(<<<JS
     gl.data['geolocation-is-not-accessible'] = 'Geolocation Not accessible.';

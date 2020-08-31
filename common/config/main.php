cda07ad0455c95331014311e7428b03450a988c6
dmitriy-dolgov <TwilightTower@mail.ru>
2020-08-31 03:41:29 +0300

    YandexMetrika removed from local

--- common/config/main.php
+++ common/config/main.php
@@ -4,17 +4,17 @@ Yii::setAlias('@root', dirname(dirname(__DIR__)));
 
 $params = require __DIR__ . '/params.php';
 
-return [
+$configData = [
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
-        '@npm'   => '@vendor/npm-asset',
+        '@npm' => '@vendor/npm-asset',
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
-        'view' => [
+        /*'view' => [
             'as YandexMetrika' => [
                 'class' => \hiqdev\yii2\YandexMetrika\Behavior::class,
                 'builder' => [
@@ -23,12 +23,25 @@ return [
                     'params' => $params['yandexMetrika.params'],
                 ],
             ],
-        ],
-        'sse' => [
+        ],*/
+        /*'sse' => [
             'class' => \odannyc\Yii2SSE\LibSSE::class,
             'sleep_time' => 7,
             'exec_limit' => 0,    //30, //???  0?   // A 30 second time limit can prevent running out of resources quickly. - send.php
             'keep_alive_time' => 17,
-        ],
+        ],*/
     ],
 ];
+
+if (!\common\helpers\Web::isLocal()) {
+    $configData['components']['view']['as YandexMetrika'] = [
+        'class' => \hiqdev\yii2\YandexMetrika\Behavior::class,
+        'builder' => [
+            'class' => \hiqdev\yii2\YandexMetrika\CodeBuilder::class,
+            'id' => $params['yandexMetrika.id'],
+            'params' => $params['yandexMetrika.params'],
+        ],
+    ];
+}
+
+return $configData;

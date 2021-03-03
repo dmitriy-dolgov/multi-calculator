26caf3069af141022c7ad5b438eb3cfcebe2f7d8
dmitriy-dolgov <TwilightTower@mail.ru>
2021-03-03 21:25:57 +0300

    String trim polyfill

--- common/share/web/js/common.js
+++ common/share/web/js/common.js
@@ -331,3 +331,13 @@ $(function () {
         init();
     });
 });
+
+// Полифил для trim()
+if (!String.prototype.trim) {
+    (function() {
+        // Вырезаем BOM и неразрывный пробел
+        String.prototype.trim = function() {
+            return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
+        };
+    })();
+}

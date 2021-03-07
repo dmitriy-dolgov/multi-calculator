645c02435d38130ce3ff220811fb5f939d31e7b7
dmitriy-dolgov <TwilightTower@mail.ru>
2021-03-07 17:04:31 +0300

    order panel unwrap panels stuff (delete button added)

--- frontend/views/site/__you_panel_elements_list.php
+++ frontend/views/site/__you_panel_elements_list.php
@@ -7,8 +7,37 @@ use yii\helpers\Html;
 ?>
 
 <div class="you-panel-elements-list unwrapped-panel">
-    <?= Html::img('/img/ok-btn.svg', ['title' => Yii::t('app', 'Свернуть'), 'class' => 'unwrap-panel__close-button']) ?>
+    <?= Html::img('/img/ok-btn.svg', [
+            'title' => Yii::t('app', 'Свернуть'),
+            'class' => 'unwrap-panel__close-button']
+    ) ?>
     <div class="header">Ваши адреса:</div>
     <hr>
-    <div class="content"></div>
+    <div class="unwrap-panel-content">
+        Адрес: Ул. Джонни Деппа
+        Тел: 8902342323
+        Email: fkjdsl@rere.com
+        <?= Html::img('/img/close-btn.svg', [
+                'title' => Yii::t('app', 'Удалить адрес'),
+                'class' => 'unwrap-panel__delete-button address']
+        ) ?>
+    </div>
+    <div class="unwrap-panel-content">
+        Адрес: Ул. Джонни Деппа 2
+        Тел: 890234232355
+        Email: fkjdsl@rere.com22
+        <?= Html::img('/img/close-btn.svg', [
+                'title' => Yii::t('app', 'Удалить адрес'),
+                'class' => 'unwrap-panel__delete-button address']
+        ) ?>
+    </div>
+    <div class="unwrap-panel-content">
+        Адрес: Ул. Джонни Деппа 3
+        Тел: 8902342323553
+        Email: fkjdsl@rere.com2233
+        <?= Html::img('/img/close-btn.svg', [
+                'title' => Yii::t('app', 'Удалить адрес'),
+                'class' => 'unwrap-panel__delete-button address']
+        ) ?>
+    </div>
 </div>

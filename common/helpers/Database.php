47435f167f82a1e73e1ac00c73fbc317a324b479
dmitriy-dolgov <TwilightTower@mail.ru>
2021-06-02 20:33:41 +0300

    fake user stuff

--- common/helpers/Database.php
+++ common/helpers/Database.php
@@ -12,4 +12,28 @@ class Database
             return null;
         }
     }
+
+    /**
+     * Проверяет таблицу $tableClass на наличие в колонке $columnName значения $columnValue.
+     * Генерирует новое уникальное значение для колонки $columnName.
+     *
+     * @param string $tableClass ActiveRecord класс
+     * @param string $columnName название поля в $tableClass
+     * @param string $columnValue значение поля $columnName
+     * @return string новое уникальное значение для колонки $columnName
+     */
+    public static function handeParameterElement(string $tableClass,
+                                                 string $columnName,
+                                                 string $columnValue = 'fake')
+    {
+        $count = 0;
+        $columnValueMod = $columnValue;
+
+        while ($tableClass::findOne([$columnName => $columnValueMod])->exists()) {
+            ++$count;
+            $columnValueMod = $columnValue . '_' . $count;
+        }
+
+        return $columnValueMod;
+    }
 }
--- common/models/repositories/UserRepository.php
+++ common/models/repositories/UserRepository.php
@@ -2,73 +2,50 @@
 
 namespace common\models\repositories;
 
+use common\helpers\Database;
 use common\models\db\User;
 use Yii;
 
 class UserRepository extends \yii\db\ActiveRecord
 {
-    /**
-     * Берет из базы данных значение параметра $name.
-     * Если нет такого значения, то генерирует для него случайное название.
-     *
-     * @param string $name название значения
-     * @param string $postfix для добааление в конец названия
-     */
-    protected static function handeParameterElement(string $name, string $postfix = '')
+    public static function createFakeUser()
     {
-        /*for (User::find()->exists([$name])) {
+        //$username, $email, $latLong, $password_ha = 'temp123')
 
-        }*/
+        $transaction = static::getDb()->beginTransaction();
 
-        //if (User::findOne(['$name' => $newUsername])) {
-    }
+        try {
+            $password = 'temp123';
+            $newUsername = Database::handeParameterElement(User::class, 'username', 'fake_name');
+            $newEmail = Database::handeParameterElement(User::class, 'email', 'fake@emal.com');
 
-    public static function createFakeUser($data)
-    {
-        $username, $email, $latLong, $password_ha = 'temp123')
+            $newUser = new User();
+            $newUser->username = $newUsername;
+            $newUser->email = $newEmail;
+            //$newUser->password_hash = '$2y$10$EXA/SoXj2Oq1Wa.lLisrl.XfhHAkA9CuID/P/.aftis2dxYlAoTI.';
+            $newUser->password_hash = Yii::$app->security->generatePasswordHash($password);
+            //$newUser->login_lat_long = $coords[$i];
+            $newUser->registration_ip = '127.0.0.1';
+            $newUser->confirmed_at = time();
+            $newUser->created_at = time();
+            $newUser->updated_at = time();
+            $newUser->auth_tf_enabled = 0;
+            $newUser->gdpr_deleted = 0;
+            $newUser->gdpr_consent = 0;
+            $newUser->order_uid = bin2hex(random_bytes(10));
+            $newUser->flags = 0;
+            //$newUser->last_login_at = '1622487545';
+            $newUser->last_login_ip = '127.0.0.1';
 
-        if (!$username) {
-            for (; ;) {
-                $username = "{fake_{$dataPrefix}";
-                // Значение, используется для генерации уникальных значений.
-                if (User::findOne(['username' => $newUsername])) {
-                    \Yii::warning("Username already exists: '{$newUsername}'");
-                }
-                if (User::findOne(['username' => $newUsername])) {
-                    \Yii::warning("Username already exists: '{$newUsername}'");
-                    continue;
-                }
+            if ($newUser->save()) {
+                Yii::info("User '{$newUsername}' created");
+            } else {
+                throw new \Exception("Couldn't create user with name '{$newUsername}'");
             }
-        }
 
-        if (User::findOne(['username' => $newUsername])) {
-            \Yii::warning("Username already exists: '{$newUsername}'");
-            continue;
-        }
-
-        $newUser = new User();
-        //$newUser->password_hash = '$2y$10$EXA/SoXj2Oq1Wa.lLisrl.XfhHAkA9CuID/P/.aftis2dxYlAoTI.';
-        $newUser->password_hash = Yii::$app->security->generatePasswordHash($password);
-        $newUser->email = "fake_{$dataPrefix}@emal.com";
-        $newUser->username = $username;
-        $newUser->login_lat_long = $coords[$i];
-        $newUser->registration_ip = '127.0.0.1';
-        $newUser->confirmed_at = time();
-        $newUser->created_at = time();
-        $newUser->updated_at = time();
-        $newUser->auth_tf_enabled = 0;
-        $newUser->gdpr_deleted = 0;
-        $newUser->gdpr_consent = 0;
-        $newUser->order_uid = bin2hex(random_bytes(10));
-        $newUser->flags = 0;
-        $newUser->last_login_at = '1622487545';
-        $newUser->last_login_ip = '127.0.0.1';
-
-        if ($newUser->save()) {
-            echo "User '{$newUsername}' created";
-        } else {
-            echo "Couldn't create user with name '{$newUsername}'";
-            return false;
+            $transaction->commit();
+        } finally {
+            $transaction->rollBack();
         }
     }
 }

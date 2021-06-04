74bf81ebdc684a891ce336a9ff432ebd3824641a
dmitriy-dolgov <TwilightTower@mail.ru>
2021-06-04 15:29:24 +0300

    courier staff

--- common/models/repositories/UserRepository.php
+++ common/models/repositories/UserRepository.php
@@ -3,11 +3,14 @@
 namespace common\models\repositories;
 
 use common\helpers\Database;
+use common\models\db\Profile;
 use common\models\db\User;
 use Yii;
 
 class UserRepository extends \yii\db\ActiveRecord
 {
+    const DEFAULT_USER_PASSWORD;
+
     /**
      * @param $companyLatLong string широта и долгота в таком шаблоне: '55.74958090241472;37.54247323613314'
      * @throws \yii\base\Exception
@@ -18,7 +21,7 @@ class UserRepository extends \yii\db\ActiveRecord
         $transaction = static::getDb()->beginTransaction();
 
         try {
-            $password = 'temp123';
+            //DEFAULT_USER_PASSWORD$password = 'temp123';
             $newUsername = Database::handeParameterElement(User::class, 'username', 'fake_name');
             $newEmail = Database::handeParameterElement(User::class, 'email', 'fake@emal.com');
 
@@ -26,7 +29,7 @@ class UserRepository extends \yii\db\ActiveRecord
             $newUser->username = $newUsername;
             $newUser->email = $newEmail;
             //$newUser->password_hash = '$2y$10$EXA/SoXj2Oq1Wa.lLisrl.XfhHAkA9CuID/P/.aftis2dxYlAoTI.';
-            $newUser->password_hash = Yii::$app->security->generatePasswordHash($password);
+            $newUser->password_hash = Yii::$app->security->generatePasswordHash(self::DEFAULT_USER_PASSWORD);
             //$newUser->login_lat_long = $coords[$i];
             $newUser->registration_ip = '127.0.0.1';
             $newUser->confirmed_at = time();
@@ -35,24 +38,32 @@ class UserRepository extends \yii\db\ActiveRecord
             $newUser->auth_tf_enabled = 0;
             $newUser->gdpr_deleted = 0;
             $newUser->gdpr_consent = 0;
-            $newUser->order_uid = bin2hex(random_bytes(10));
+            //$newUser->order_uid = bin2hex(random_bytes(10));
+            $newUser->order_uid = Database::handeParameterElement(User::class, 'order_uid', bin2hex(random_bytes(10)));
             $newUser->flags = 0;
             //$newUser->last_login_at = '1622487545';
             //$newUser->last_login_ip = '127.0.0.1';
 
             if ($newUser->save()) {
                 Yii::info("User '{$newUsername}' created");
-                $newUser->profile->company_lat_long = $companyLatLong;
-                echo $newUser->getProfile()->company_lat_long;
-                echo "+++++++++++++++++++++++++++++++++++++++++++++++++++\n";
-                $uu = $newUser->getProfile();
-                print_r($uu);
-                exit;
+
+                /** @var $newUserProfile Profile */
+                $newUserProfile = $newUser->profile;
+                $newUserProfile->name = Yii::t('app', 'Не указано');
+                $newUserProfile->timezone = 'Europe/Moscow';
+                $newUserProfile->company_lat_long = $companyLatLong;
+                if ($newUserProfile->save()) {
+                    Yii::info("User '{$newUsername}' created");
+                } else {
+                    throw new \Exception("Couldn't create User's profile. Username: '{$newUsername}'");
+                }
             } else {
                 throw new \Exception("Couldn't create user with name '{$newUsername}'");
             }
 
             $transaction->commit();
+        } catch (\Throwable $e) {
+            throw $e;
         } finally {
             $transaction->rollBack();
         }
--- console/config/main.php
+++ console/config/main.php
@@ -30,6 +30,65 @@ return [
                 ],
             ],
         ],
+        'user' => [
+            'class' => 'common\models\db\User',
+            //'identityClass' => 'common\models\db\User',
+            //'enableAutoLogin' => true,
+            //'identityCookie' => ['name' => '_identity-frontend', 'httpOnly' => true],
+        ],
+    ],
+    'modules' => [
+        'user' => [
+            'class' => \Da\User\Module::class,
+            //'generatePasswords' => true,
+            //TODO: разобраться что это точно
+            'switchIdentitySessionKey' => 'ladlen_daiviz_undula',
+
+            'classMap' => [
+                'User' => \common\models\db\User::class,
+                'Profile' => \common\models\db\Profile::class,
+                'ResendForm' => \common\models\forms\ResendForm::class,
+            ],
+
+            //'layout' => '@backend/modules/setup/views/layouts/main',
+
+            'controllerMap' => [
+                //'settings' => 'backend\modules\setup\controllers\SettingsController',
+                'security' => [
+                    //'class' => \Da\User\Controller\SecurityController::class,
+                    'class' => 'common\controllers\SecurityController',
+                    'layout' => '@frontend/views/layouts/registration',
+                    'on ' . \Da\User\Event\FormEvent::EVENT_AFTER_LOGIN => function (\Da\User\Event\FormEvent $event) {
+                        \Yii::$app->controller->redirect(['/site/signal-to-parent', 'result' => 'logged']);
+                        \Yii::$app->end();
+                    },
+                    'on ' . \Da\User\Event\SocialNetworkAuthEvent::EVENT_AFTER_AUTHENTICATE => function ($event) {
+                        \Yii::$app->controller->redirect(['/site/signal-to-parent-opener', 'result' => 'logged']);
+                        \Yii::$app->end();
+                    },
+                ],
+                'registration' => [
+                    'class' => \Da\User\Controller\RegistrationController::class,
+                    'layout' => '@frontend/views/layouts/registration',
+                ],
+                /*'admin' => [
+                    'class' => \Da\User\Controller\AdminController::class,
+                    'layout' => '@backend/modules/admin/views/layouts/main',
+                ],
+                'role' => [
+                    'class' => \Da\User\Controller\RoleController::class,
+                    'layout' => '@backend/modules/admin/views/layouts/main',
+                ],
+                'permission' => [
+                    'class' => \Da\User\Controller\PermissionController::class,
+                    'layout' => '@backend/modules/admin/views/layouts/main',
+                ],
+                'rule' => [
+                    'class' => \Da\User\Controller\RuleController::class,
+                    'layout' => '@backend/modules/admin/views/layouts/main',
+                ],*/
+            ],
+        ],
     ],
     'params' => $params,
 ];

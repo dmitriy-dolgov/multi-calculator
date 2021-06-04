0542c424746ba1a8d3c77ab9eef5c2d91c00747b
dmitriy-dolgov <TwilightTower@mail.ru>
2021-06-04 18:27:19 +0300

    fake users stuff

--- common/models/repositories/UserRepository.php
+++ common/models/repositories/UserRepository.php
@@ -9,21 +9,43 @@ use Yii;
 
 class UserRepository extends \yii\db\ActiveRecord
 {
-    const DEFAULT_USER_PASSWORD;
+    const DEFAULT_USER_PASSWORD = 'temp123';
+
+
+    public static function createFakeUsers()
+    {
+        $coords = [
+            '55.74958090241472;37.54247323613314',
+            '55.78863136588515;37.44675988832758',
+            '55.84568433042044;37.729379871438084',
+            '55.75095122759028;37.68105308990117',
+            '55.72115988400856;37.48774590609692',
+            '55.891548932356905;37.67008469607761',
+            '55.76078841987557;37.59588219204036',
+            '55.836437940564416l37.434382624429865',
+            '55.761197731327755;37.523134639062654',
+            '55.80169830229568;37.76320156388906',
+        ];
+        $coordsCount = count($coords);
+
+        for ($i = 0; $i < $coordsCount; ++$i) {
+            UserRepository::createAFakeUser($coords[$i]);
+        }
+    }
 
     /**
-     * @param $companyLatLong string широта и долгота в таком шаблоне: '55.74958090241472;37.54247323613314'
-     * @throws \yii\base\Exception
-     * @throws \yii\db\Exception
+     * @param string $companyLatLong string широта и долгота
+     *      в таком шаблоне: '55.74958090241472;37.54247323613314'
+     * @throws \Throwable
      */
-    public static function createFakeUser(string $companyLatLong)
+    public static function createAFakeUser(string $companyLatLong)
     {
         $transaction = static::getDb()->beginTransaction();
 
         try {
             //DEFAULT_USER_PASSWORD$password = 'temp123';
             $newUsername = Database::handeParameterElement(User::class, 'username', 'fake_name');
-            $newEmail = Database::handeParameterElement(User::class, 'email', 'fake@emal.com');
+            $newEmail = Database::handeParameterElement(User::class, 'email', 'fake@email.com');
 
             $newUser = new User();
             $newUser->username = $newUsername;
@@ -48,12 +70,17 @@ class UserRepository extends \yii\db\ActiveRecord
                 Yii::info("User '{$newUsername}' created");
 
                 /** @var $newUserProfile Profile */
-                $newUserProfile = $newUser->profile;
+                $newUserProfile = $newUser->make(Profile::class);
+                $newUserProfile->name = Yii::t('app', 'Нет имени');
+                $newUserProfile->timezone = 'Europe/Moscow';   //TODO при выхода за пределы Москвы - решить
                 $newUserProfile->name = Yii::t('app', 'Не указано');
                 $newUserProfile->timezone = 'Europe/Moscow';
                 $newUserProfile->company_lat_long = $companyLatLong;
+
+                $newUserProfile->link('user', $newUser);
+
                 if ($newUserProfile->save()) {
-                    Yii::info("User '{$newUsername}' created");
+                    Yii::info("Profile created for user '{$newUsername}' created");
                 } else {
                     throw new \Exception("Couldn't create User's profile. Username: '{$newUsername}'");
                 }
@@ -62,6 +89,8 @@ class UserRepository extends \yii\db\ActiveRecord
             }
 
             $transaction->commit();
+        } catch (\Exception $e) {
+            throw $e;
         } catch (\Throwable $e) {
             throw $e;
         } finally {
